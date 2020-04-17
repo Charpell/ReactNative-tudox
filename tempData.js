a03c1bcca9a26556b4b502eb9542238fc6213a8c
Ebuka Umeh <ebukaumeh@Ebukas-MacBook-Pro.local>
2020-04-17 18:26:21 +0100

    todo

---
+++ tempData.js
@@ -0,0 +1,68 @@
+export default tempdata = [
+    {
+        id: 1,
+        name: "Plan a trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage"
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Birthday Party",
+        color: "#595BD9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage"
+            }
+        ]
+    }
+]
